fbb4146dc6e09abcc107f5bdba80e8278068b829
Burgas <burgassme@gmail.com>
2026-06-05 21:47:44 +0700

    Initial commit

--- src/main/java/org/burgas/trainingservice/router/SecurityRouter.java
+++ src/main/java/org/burgas/trainingservice/router/SecurityRouter.java
@@ -7,14 +7,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.RouterFunctions;
 import org.springframework.web.servlet.function.ServerResponse;
-
-import java.util.Objects;
 
 @Configuration
 @RequiredArgsConstructor
@@ -31,8 +28,9 @@ public class SecurityRouter {
                         })
 
                         .GET("/login", _ -> {
-                            SecurityContext securityContext = SecurityContextHolder.getContext();
-                            if (Objects.requireNonNull(securityContext.getAuthentication()).isAuthenticated()) {
+                            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                            assert authentication != null;
+                            if (authentication.isAuthenticated()) {
                                 return ServerResponse.ok().body("You successfully logged in");
                             } else {
                                 throw new IllegalArgumentException("Not authenticated");
@@ -43,13 +41,12 @@ public class SecurityRouter {
                             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
                             assert authentication != null;
                             if (authentication.isAuthenticated()) {
-                                authentication.setAuthenticated(false);
                                 SecurityContextHolder.clearContext();
                                 HttpSession session = request.session();
                                 session.removeAttribute("SPRING_SECURITY_CONTEXT");
                                 return ServerResponse.ok().body("You successfully logged out");
                             } else {
-                                return ServerResponse.ok().body("You are not authenticated for logout");
+                                throw new IllegalArgumentException("You are not authenticated for logout");
                             }
                         })
 
