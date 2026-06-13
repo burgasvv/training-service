a00e29de213e613e26846c4f789cee8930378093
Burgas <burgassme@gmail.com>
2026-06-13 19:59:18 +0700

    Handle redis keys

--- src/main/java/org/burgas/trainingservice/redis/RedisKeys.java
+++ src/main/java/org/burgas/trainingservice/redis/RedisKeys.java
@@ -2,7 +2,7 @@ package org.burgas.trainingservice.redis;
 
 public class RedisKeys {
 
-    public static final String identityKey = "identity%s";
-    public static final String courseKey = "course%s";
-    public static final String projectKey = "project%s";
+    public static final String identityKey = "identity::%s";
+    public static final String courseKey = "course::%s";
+    public static final String projectKey = "project::%s";
 }
